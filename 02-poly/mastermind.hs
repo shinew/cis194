6a09b50af82c9875d7690b4eaaf36ceb77f213e5
Shine Wang <shine@getbeyond.me>
2015-04-07 17:46:53 -0400

    saving work for Mastermind

---
+++ 02-poly/mastermind.hs
@@ -0,0 +1,71 @@
+{-# OPTIONS_GHC -Wall #-}
+module HW02 where
+
+-- Mastermind -----------------------------------------
+
+-- A peg can be one of six colors
+data Peg = Red | Green | Blue | Yellow | Orange | Purple
+         deriving (Show, Eq, Ord)
+
+-- A code is defined to simply be a list of Pegs
+type Code = [Peg]
+
+-- A move is constructed using a Code and two integers; the number of
+-- exact matches and the number of regular matches
+data Move = Move Code Int Int
+          deriving (Show, Eq)
+
+-- List containing all of the different Pegs
+colors :: [Peg]
+colors = [Red, Green, Blue, Yellow, Orange, Purple]
+
+-- Exercise 1 -----------------------------------------
+
+-- Get the number of exact matches between the actual code and the guess
+exactMatches :: Code -> Code -> Int
+exactMatches secret guess = length $ filter (\(a,b) -> a == b) $ zip secret guess
+
+-- Exercise 2 -----------------------------------------
+
+-- For each peg in xs, count how many times is occurs in ys
+countColors :: Code -> [Int]
+countColors code = map (\c -> count c code) colors
+    where count c d = length $ filter (==c) d
+
+-- Count number of matches between the actual code and the guess
+matches :: Code -> Code -> Int
+matches secret guess = sum $ map comp $ zip (countColors secret) (countColors guess)
+    where comp (s,g) = min s g
+
+-- Exercise 3 -----------------------------------------
+
+-- Construct a Move from a guess given the actual code
+getMove :: Code -> Code -> Move
+getMove secret guess = Move guess exact (allMatches - exact)
+    where exact = exactMatches secret guess
+          allMatches = matches secret guess
+
+-- Exercise 4 -----------------------------------------
+
+isConsistent :: Move -> Code -> Bool
+isConsistent move@(Move guess _ _) secret  = getMove secret guess == move
+
+-- Exercise 5 -----------------------------------------
+
+filterCodes :: Move -> [Code] -> [Code]
+filterCodes = undefined
+
+-- Exercise 6 -----------------------------------------
+
+allCodes :: Int -> [Code]
+allCodes = undefined
+
+-- Exercise 7 -----------------------------------------
+
+solve :: Code -> [Move]
+solve = undefined
+
+-- Bonus ----------------------------------------------
+
+fiveGuess :: Code -> [Move]
+fiveGuess = undefined
