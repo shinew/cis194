5545b289a46da7f8b342fecd39e6209e2cfcfc99
Shine Wang <shine@getbeyond.me>
2015-10-01 00:38:26 -0400

    halfway through 05

---
+++ 05-IO/Parser.hs
@@ -0,0 +1,34 @@
+{-# LANGUAGE RecordWildCards, OverloadedStrings #-}
+module Parser ( encode
+              , decode
+              , Transaction(..)
+              , TId
+              , FromJSON(..)
+              , ToJSON(..)
+              ) where
+
+import Data.Aeson
+
+type TId = String
+
+data Transaction = Transaction { from   :: String
+                               , to     :: String
+                               , amount :: Integer
+                               , tid    :: TId
+                               }
+                   deriving (Show, Eq)
+
+instance FromJSON Transaction where
+    parseJSON (Object v) = Transaction   <$>
+                           v .: "from"   <*>
+                           v .: "to"     <*>
+                           v .: "amount" <*>
+                           v .: "tid"
+    parseJSON _ = mempty
+
+instance ToJSON Transaction where
+    toJSON Transaction{..} = object [ "from"   .= from
+                                    , "to"     .= to
+                                    , "amount" .= amount
+                                    , "tid"    .= tid
+                                    ]
--- 05-IO/homework.hs
+++ 05-IO/homework.hs
@@ -1,9 +1,12 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 import qualified Data.ByteString as BS
+import qualified Data.ByteString.Lazy as BSL
 import qualified Data.ByteString.Char8 as BS.Char8
 import Data.ByteString (ByteString)
 import Data.Bits
+
+import Parser
 
 getSecret :: FilePath -> FilePath -> IO ByteString
 getSecret original modified =
@@ -24,9 +27,29 @@ decryptWithKey key file =
     let unpackedJson = BS.unpack json
     BS.writeFile file $ processKey unpackedJson longKey
 
+parseFile :: FromJSON a => FilePath -> IO (Maybe a)
+parseFile file =
+  do
+    contents <- BSL.readFile file
+    return $ decode contents
+
+filterTransactions :: Maybe [TId] -> Maybe [Transaction] -> Maybe [Transaction]
+filterTransactions (Just vs) (Just ts) =
+  Just $ filter (\t -> any (\v -> tid t == v) vs) ts
+filterTransactions _ _ = Nothing
+
+getBadTs :: FilePath -> FilePath -> IO (Maybe [Transaction])
+getBadTs victimFile transactionFile =
+  do
+    victims <- parseFile victimFile :: IO (Maybe [TId])
+    transactions <- parseFile transactionFile :: IO (Maybe [Transaction])
+    return $ filterTransactions victims transactions
+
 main :: IO ()
 main =
   do
     secret <- getSecret "dog-original.jpg" "dog.jpg"
     BS.Char8.putStrLn secret
     decryptWithKey secret "victims.json"
+    _ <- getBadTs "victims.json" "transactions.json"
+    putStrLn "Enough"
